30f05d0b014bb8f80eae530b31cb5610798198d0
Naiabunner <Okechukwudaniel12@gmail.com>
2024-03-08 19:08:20 +0100

    Last commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const Gridarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 34, 5, 6, 7, 8, 9];
   function transitionGenerator() {
     if (count === 1) {
       return "tansition_one";
@@ -16,18 +19,31 @@ function App() {
       return "transition_five ";
     }
   }
-  const [count, setCount] = useState(0);
+  const play = () => {
+    let i = count;
+    const delay = 1000; 
+  
+    const intervalId = setInterval(() => {
+      if (i >= 6) {
+        clearInterval(intervalId);
+      } else {
+        setCount(i);
+        i++;
+      }
+    }, delay);
+  };
+    
+  
   const counter = () => {
     if (count < 5) {
       setCount((cou) => cou + 1);
     }
   };
-  const Gridarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 34, 5, 6, 7, 8, 9];
-
   return (
     <>
       <div className="btn_wrapper " id="top">
         <button onClick={counter}> transition {count}</button>
+        <button onClick={play}> Play all <br />{count}</button>
         <button onClick={() => setCount(0)}> refresh</button>
         <button>
           <a href="#blank">blank page</a>{" "}
