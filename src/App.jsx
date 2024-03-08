bee62b3f02b5049ca14e8a1b7b278d7af5fc1eae
Naiabunner <Okechukwudaniel12@gmail.com>
2024-03-08 18:51:48 +0100

    first commit last commi

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  function transitionGenerator() {
+    if (count === 1) {
+      return "tansition_one";
+    } else if (count === 2) {
+      return "transition_two";
+    } else if (count === 3) {
+      return "transition_three";
+    } else if (count === 4) {
+      return "transition_four ";
+    } else if (count === 5) {
+      return "transition_five ";
+    }
+  }
+  const [count, setCount] = useState(0);
+  const counter = () => {
+    if (count < 5) {
+      setCount((cou) => cou + 1);
+    }
+  };
+  const Gridarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 34, 5, 6, 7, 8, 9];
+
+  return (
+    <>
+      <div className="btn_wrapper " id="top">
+        <button onClick={counter}> transition {count}</button>
+        <button onClick={() => setCount(0)}> refresh</button>
+        <button>
+          <a href="#blank">blank page</a>{" "}
+        </button>
+      </div>
+
+      <main className={`table ${transitionGenerator()}`}>
+        <div className="genbox">
+          <div className="icon_1">{count > 2 ? "↓" : "↑"}</div>
+          <div className="icon_2">{count !== 0 ? "↓" : "Hi"}</div>
+        </div>
+        <div className="gen2 ">
+          <div className="icon_3">{count > 1 ? "↑" : "Hello"}</div>
+          <div className="icon_4">👱 </div>
+          <p className="note">Thank You</p>
+        </div>
+
+        {Gridarr.map((entries, index) => {
+          return <div key={index} className="box transition-three"></div>;
+        })}
+      </main>
+      <section id="blank" className="">
+        <button>
+          <a href="#top">top</a>{" "}
+        </button>
+      </section>
+    </>
+  );
+}
+
+export default App;
